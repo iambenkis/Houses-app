91bc4242b8f5f148786a983eb3ab5ae63179e7f9
benjamin kisenge <benkisenge03@gmail.com>
2022-05-26 22:12:53 +0200

    popup added

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 let loadMore = document.querySelector('.more');
 let crntitem = 4;
 let items = document.querySelectorAll('.item');
+let pops = document.querySelectorAll('.popup-content');
+let container = document.querySelector('.modal-container')
+let leaves = document.querySelectorAll('.leave')
  
 // menu deroulant 
 
 let menu = document.querySelector('.menu')
 let nav_menu = document.querySelector('.nav-list')
+let imgs = document.querySelectorAll('.item-img')
+let description = document.querySelectorAll('.description')
 let ismore = 0;
 
 loadMore.addEventListener('click',() => { 
@@ -29,4 +34,24 @@ menu.addEventListener('click',() => {
         nav_menu.style.display = 'none'
 })
 
+imgs.forEach((img ,index )=> {
+        img.addEventListener('click',function(){ 
+           pops[index].classList.add('active') 
+           container.classList.add('active')
+        })
+} )
+
+const leavePop = () => {
+        pops.forEach(pop => {
+                pop.classList.remove('active')
+                container.classList.remove('active');
+        })
+}
+
+leaves.forEach(leave =>{
+        leave.addEventListener('click',leavePop);
+})
+
+
+ 
  
